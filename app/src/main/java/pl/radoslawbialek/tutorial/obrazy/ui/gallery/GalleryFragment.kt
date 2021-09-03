8eab782d65bec121795385377b0f54e322e38d44
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-09-03 17:14:47 +0200

    Dodanie ui/gallery/GalleryFragment

---
+++ app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package pl.radoslawbialek.tutorial.obrazy.ui.gallery
+
+import androidx.fragment.app.Fragment
+import pl.radoslawbialek.tutorial.obrazy.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
