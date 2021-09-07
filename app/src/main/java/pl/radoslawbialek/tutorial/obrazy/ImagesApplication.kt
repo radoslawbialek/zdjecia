6dac4bc64fed8963378a3ab51c663d2cb4436716
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-09-07 15:27:29 +0200

    Utworzenie ImagesApplication i dodanie adnotacji indziej

---
+++ app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ImagesApplication.kt
@@ -0,0 +1,8 @@
+package pl.radoslawbialek.tutorial.obrazy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ImagesApplication : Application() {
+}
--- app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,9 @@
 package pl.radoslawbialek.tutorial.obrazy.ui.gallery
 
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import pl.radoslawbialek.tutorial.obrazy.R
 
+@AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 }
