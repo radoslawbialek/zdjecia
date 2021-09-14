e0375dcc9f67fc3d3b27db9451b21b61c2b60ee6
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-09-14 16:27:15 +0200

    Uzupełnienie GalleryFragment i wyświetlenie obrazów

--- app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ui/gallery/GalleryFragment.kt
@@ -6,17 +6,35 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import pl.radoslawbialek.tutorial.obrazy.R
+import pl.radoslawbialek.tutorial.obrazy.databinding.FragmentGalleryBinding
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel>()
 
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
+        _binding = FragmentGalleryBinding.bind(view)
 
+        val adapter = UnsplashPhotoAdapter()
+
+        binding.apply {
+            galleryRecyclerView.setHasFixedSize(true)
+            galleryRecyclerView.adapter = adapter
         }
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
