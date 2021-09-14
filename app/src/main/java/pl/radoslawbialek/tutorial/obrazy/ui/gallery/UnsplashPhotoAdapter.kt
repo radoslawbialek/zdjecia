4315d97624eb8a064cc48b3c7771a02db2e4e5ba
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-09-14 14:26:55 +0200

    Utworzenie UnsplashPhotoAdapter i usunięcie InvocationTargetException

---
+++ app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ui/gallery/UnsplashPhotoAdapter.kt
@@ -0,0 +1,54 @@
+package pl.radoslawbialek.tutorial.obrazy.ui.gallery
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import pl.radoslawbialek.tutorial.obrazy.R
+import pl.radoslawbialek.tutorial.obrazy.data.UnsplashPhoto
+import pl.radoslawbialek.tutorial.obrazy.databinding.ItemPhotoBinding
+
+class UnsplashPhotoAdapter : PagingDataAdapter<UnsplashPhoto, UnsplashPhotoAdapter.PhotoViewHolder>(PHOTO_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val binding = ItemPhotoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        if (currentItem != null) {
+            holder.bind(currentItem)
+        }
+    }
+
+    class PhotoViewHolder(private val binding: ItemPhotoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(photo: UnsplashPhoto) {
+            binding.apply {
+                Glide.with(photoImageView)
+                    .load(photo.urls.regular)
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .error(R.drawable.ic_baseline_error_outline_24)
+                    .into(photoImageView)
+
+                userTextView.text = photo.user.username
+            }
+        }
+    }
+
+    companion object {
+        private val PHOTO_COMPARATOR = object : DiffUtil.ItemCallback<UnsplashPhoto>() {
+            override fun areItemsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto) =
+                oldItem == newItem
+        }
+    }
+}
