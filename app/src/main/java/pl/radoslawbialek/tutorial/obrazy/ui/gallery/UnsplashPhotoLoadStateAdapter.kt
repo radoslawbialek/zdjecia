b3c22b941d84c0745ef3ada8a7156098c07e7609
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-09-14 17:32:49 +0200

    Dodanie UnsplashPhotoLoadStateAdapter

---
+++ app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ui/gallery/UnsplashPhotoLoadStateAdapter.kt
@@ -0,0 +1,40 @@
+package pl.radoslawbialek.tutorial.obrazy.ui.gallery
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import pl.radoslawbialek.tutorial.obrazy.databinding.UnsplashPhotoLoadStateFooterBinding
+
+class UnsplashPhotoLoadStateAdapter(private val retry: () -> Unit) :
+    LoadStateAdapter<UnsplashPhotoLoadStateAdapter.LoadStateViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): LoadStateViewHolder {
+        val binding = UnsplashPhotoLoadStateFooterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return LoadStateViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: LoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    inner class LoadStateViewHolder(private val binding: UnsplashPhotoLoadStateFooterBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.retryButton.setOnClickListener {
+                retry.invoke()
+            }
+        }
+
+        fun bind(loadState: LoadState) {
+            binding.apply {
+                progressBar.isVisible = loadState is LoadState.Loading
+                retryButton.isVisible = loadState !is LoadState.Loading
+                errorTextView.isVisible = loadState !is LoadState.Loading
+            }
+        }
+    }
+}
