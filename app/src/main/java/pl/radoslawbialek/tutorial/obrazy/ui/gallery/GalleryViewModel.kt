e8940147f30bcf83b015402a14a86cb5be7bd201
Radosław Białek <radoslaw@radoslawbialek.pl>
2021-09-15 18:13:08 +0200

    Zapisanie bieżącego wyszukiwania w GalleryViewModel

--- app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/pl/radoslawbialek/tutorial/obrazy/ui/gallery/GalleryViewModel.kt
@@ -1,7 +1,8 @@
 package pl.radoslawbialek.tutorial.obrazy.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
@@ -9,9 +10,10 @@ import androidx.paging.cachedIn
 import pl.radoslawbialek.tutorial.obrazy.data.UnsplashRepository
 
 class GalleryViewModel @ViewModelInject constructor(
-    private val repository: UnsplashRepository
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
 ) : ViewModel() {
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResult(queryString).cachedIn(viewModelScope)
@@ -23,5 +25,6 @@ class GalleryViewModel @ViewModelInject constructor(
 
     companion object {
         private const val DEFAULT_QUERY = "cats"
+        private const val CURRENT_QUERY = "current_query"
     }
 }
